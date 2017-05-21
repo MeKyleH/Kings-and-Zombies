0da16da80368307d31a137eb2b8a334ed54fb170
kholt <kyle.holt@sirsidynix.com>
2017-05-21 13:56:46 -0600

    Adding level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public float autoLoadNextLevelAfter;
+    public string autoLoadScene;
+
+    void Start()
+    {
+        if (autoLoadNextLevelAfter <= 0)
+        {
+            Debug.Log("Level auto load disabled, use a positive number in seconds");
+        }
+        else
+        {
+            Invoke(autoLoadScene != "" ? "AutoLoadScene" : "LoadNextLevel", autoLoadNextLevelAfter);
+        }
+    }
+
+    public void LoadLevel(string name)
+    {
+        Debug.Log("New Level load: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void AutoLoadScene()
+    {
+        Debug.Log("New Level load: " + autoLoadScene);
+        SceneManager.LoadScene(autoLoadScene);
+    }
+
+    public void QuitRequest()
+    {
+        Debug.Log("Quit requested");
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(Application.loadedLevel + 1);
+    }
+
+    public void StartNewGame()
+    {
+
+    }
+}
